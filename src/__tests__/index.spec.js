685f25c0377a3447d9669487ba5ad0988f76d073
Rich Lewis <richlewis42@users.noreply.github.com>
2018-12-31 15:41:16 +0000

    test: add first test case
    Adds a test case to run remark and generate a citation.

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,7 +1,26 @@
-import lib from './index'
+import cite from '../index'
+import parse from 'remark-parse'
 
-describe('index', () => {
-  it('works', () => {
-    expect(lib).toEqual({})
+describe('remark-cite', () => {
+  it('processes an in-text citation', () => {
+    const res = parse.use(cite).parse('@test')
+    expect(res).toMatchObject({
+      children: [
+        {
+          children: [
+            {
+              children: {
+                type: 'text',
+                url: '#test',
+                value: 'test'
+              },
+              type: 'link'
+            }
+          ],
+          type: 'paragraph'
+        }
+      ],
+      type: 'root'
+    })
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export default {}
+function locator(value, fromIndex) {
+  return value.indexOf('@', fromIndex)
+}
+
+function tokenizeCitation(eat, value, silent) {
+  const match = /@(\w+)/.exec(value)
+
+  if (match) {
+    if (silent) {
+      return true
+    }
+  }
+
+  return eat(match[0])({
+    type: 'link',
+    children: {
+      type: 'text',
+      url: '#' + match[1],
+      value: match[1]
+    }
+  })
+}
+
+tokenizeCitation.locator = locator
+
+export default function() {
+  const Parser = this.Parser
+  const tokenizers = Parser.prototype.inlineTokenizers
+  const methods = Parser.prototype.inlineMethods
+  tokenizers.citation = tokenizeCitation
+  methods.splice(methods.indexOf('link'), 0, 'citation')
+}
